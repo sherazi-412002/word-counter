b8139b306986f0ad55b682c53071b9dce1496013
Syed Shoaib Sherazi <syedshoaibsherazi412002@gmail.com>
2024-05-04 22:14:42 +0500

    first commit of word counter

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+// Import the 'inquirer' module
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log("\t","======================================");
+console.log(chalk.magenta("\t","<<<<< WELCOME TO WORD COUNTER >>>>>>"));
+console.log("\t","======================================");
+// Declare a constant 'answer' and assign it to the result of inquirer.promt function
+
+const answer : {
+    sentence: string
+} = await inquirer.prompt([
+    {
+       name:"sentence",
+       type:"input",
+       message:chalk.yellow("\t","Enter your para to count the words:")
+
+    }
+])
+
+const words = answer.sentence.trim().split(" ");
+
+
+
+console.log(chalk.red(`\n\t Your para is consist of ${words.length} words.`));
+
+
